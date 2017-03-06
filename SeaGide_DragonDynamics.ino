09f317def2b8d24c52e95bae02660ee15e342e37
Louie Fozard <manxboytheboy@gmail.com>
2017-03-06 10:05:30 -0500

    created main loop
    comment spellchecking
    added depth limits

--- SeaGide_DragonDynamics.ino
+++ SeaGide_DragonDynamics.ino
@@ -7,7 +7,8 @@
 static byte servoRetractCommand = 0; //retract
 static byte servoExtendCommand = 180; //extend
 int riseDriveTime = 15500; //DO NOT GO OVER 15500!! THIS WILL CAUSE THE SERVO TO PUSH THE PLUNGER "THROUGH" THE BOYANCY ENGINE
-int requiredDepthInInches = 6;
+int lowerDepth = 6; //in inches (72 = 6 feet)
+int upperDepth = 6; //in inches (72 = 6 feet)
 
 //Pins
 static byte ENGINE_PIN = 10; //Continuious rotation servo
@@ -15,7 +16,7 @@ static byte LIMIT_SWITCH = 11;
 static byte R_LED = 9;
 static byte G_LED = 6;
 static byte B_LED = 5;
-static byte LED_BASE = 7;
+static byte LED_BASE = 7; //4 pin led
 static byte PRESSURE_SENSOR = 2;
 //
 
@@ -29,23 +30,31 @@ void setup() {
   
   Serial.begin(9600);
 
-  setMotorPosition("retracted");
-  while(getDepth < requiredDepthInInches) {
-    //
-    Serial.println("depth: " + getDepth());
-  }
-  setMotorPosition("extended");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  setMotorPosition("retracted");
+
+  while(getDepth() < lowerDepth) {
+    //
+    Serial.println("depth: " + getDepth());
+  }
+
+  setMotorPosition("extended");
+
+  while(getDepth() < upperDepth) {
+    //
+    Serial.println("depth: " + getDepth());
+  }
 
 }
 
 void setMotorPosition(String destination) {
   //
   if (destination == "extended") {
-      //riseing
+      //rising
 engineMotor.attach(ENGINE_PIN);
 engineMotor.write(servoExtendCommand);
 
